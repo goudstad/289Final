ea464d0b046c18b1dc77682605bf4dec900b70dc
goudstad <goudstad@gmail.com>
2019-05-16 00:18:29 -0700

    Created models, and migrated db

--- om/hist/models.py
+++ om/hist/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Decision (models.Model):
+    title = models.CharField(max_length=256, blank=True)
+    decisions = models.TextField(blank=True)
+    decisionDate = models.DateField(auto_now_add=True)
+    backgroundInfo = models.TextField(blank=True)
+    rationale = models.TextField(blank = True)
+    subject = models.CharField(max_length=256, blank=True)
+    decisionMaker = models.ForeignKey('Employee', on_delete=models.CASCADE, blank=True, null=True)
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=256)
+    last_name = models.CharField(max_length=256)
+    position = models.CharField(max_length=256)
+
+
