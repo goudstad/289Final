a8cd1300984bc0e2b2cfd7e7f19e83b0a57b697c
goudstad <goudstad@gmail.com>
2019-05-16 20:00:46 -0700

    Overwrote print function. Added fixtures.

--- om/hist/models.py
+++ om/hist/models.py
@@ -8,11 +8,21 @@ class Decision (models.Model):
     backgroundInfo = models.TextField(blank=True)
     rationale = models.TextField(blank = True)
     subject = models.CharField(max_length=256, blank=True)
+    # define one-to-many relationship
     decisionMaker = models.ForeignKey('Employee', on_delete=models.CASCADE, blank=True, null=True)
+    
+    # override default print command
+    def __repr__(self):
+    	return '<Decision {}: {}>'.format(self.id,self.title)
+
 
 class Employee(models.Model):
     first_name = models.CharField(max_length=256)
     last_name = models.CharField(max_length=256)
     position = models.CharField(max_length=256)
+
+    # override default print command
+    def __repr__(self):
+    	return '<Employee {}: {} {}>'.format(self.id, self.first_name, self.last_name)
 
 
