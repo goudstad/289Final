01d06762780adb72b02a0039a8017ab9a84775f9
goudstad <goudstad@gmail.com>
2019-05-16 23:03:36 -0700

    modified travis.yml and test.py

--- om/hist/tests.py
+++ om/hist/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from hist.models import *
 
-# Create your tests here.
+class ModelsReprTestCase(TestCase):
+	
+	# decorator
+	@classmethod
+	def setUpTestData(cls):
+		cls.employee1 = Employee.objects.create(last_name='Beck', first_name='Tony' )
+
+	def testEmployeeRepr(self):
+		self.assertEqual(repr(self.employee1), '<Employee 1: Tony Beck>')
+
