9a7e10ee0f1159a4096856b7304d78769b3b90fa
goudstad <goudstad@gmail.com>
2019-05-20 00:13:10 -0700

    Added test for the 'Decision' model

--- om/hist/tests.py
+++ om/hist/tests.py
@@ -1,13 +1,18 @@
 from django.test import TestCase
 from hist.models import *
+from datetime import date
 
 class ModelsReprTestCase(TestCase):
 	
 	# decorator
 	@classmethod
 	def setUpTestData(cls):
-		cls.employee1 = Employee.objects.create(lname='Beck', fname='Tony' )
+		cls.employee1 = Employee.objects.create(lname='Beck', fname='Tony')
+		cls.decision1 = Decision.objects.create(title='Decision Test', decisionDesc='This is a test of the decision description field.')
 
 	def testEmployeeRepr(self):
 		self.assertEqual(repr(self.employee1), '<Employee 1: Tony Beck>')
+
+	def testDecisionRepr(self):
+		self.assertEqual(repr(self.decision1), '<Decision 1: Decision Test - This is a test of the decision description field.>')
 
